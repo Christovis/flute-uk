2a64dfb5de23bc3d5328ca0689f85be5af82c93f
Richard Bower <r.g.bower@durham.ac.uk>
2020-04-29 12:03:23 +0100

    make it easier to alternatte between UK runs and NorthEast runs

--- quick_plot.py
+++ quick_plot.py
@@ -11,15 +11,21 @@ import numpy.random as R
 #flute_dir="./"
 
 region = "northeast"
+#region="ew"
 if region == "northeast":
-    #fflute_dir= ["northeast-noaction/test_r0_1p0/","northeast-noaction/test_r0_1p5/","northeast-noaction/test_r0_2p0/","northeast-noaction/test_r0_3p0/"]
-    flute_dir= "northeast-noaction/test_r0_1p0/"
-    tract_file = flute_dir + "northeast_tracts"
-    log_file = flute_dir + "northeast_log"
+    #flute_dir= ["northeast-noaction/test_r0_1p0/","northeast-noaction/test_r0_1p5/","northeast-noaction/test_r0_2p0/","northeast-noaction/test_r0_3p0/"]
+    #flute_dir= "northeast-noaction/test_r0_1p5/"
+    flute_dir= "northeast-lockdown/"
+    tract_file = "northeast-noaction/test_r0_1p5/" + "northeast_tracts"
+    log_file = "northeast_log"
+    nomis_dir = "output_analysis/NorthEast/"
+    old_file_type=False
 else :
-    flute_dir= "ew-noaction/test_r0_1p5/"
-    tract_file = flute_dir + "ew_tracts"
-    log_file = flute_dir + "ew_log"
+    flute_dir= "ew-noaction/test_r0_2p0/"
+    tract_file = "ew-noaction/test_r0_2p0/" + "ew_tracts"
+    log_file = "ew_log"
+    nomis_dir = "output_analysis/EnglandWales/"
+    old_file_type=True
 
 home_dir="./"
 
@@ -28,7 +34,8 @@ total_pop = N.array([158627,521126,343960,1279055,355487])
 
 # fraction of people that show symptoms. Needs to be read from input file!
 # symptomatic_fraction = 0.58
-infection_length = 
+n_infected_days = 14  # the number of days in the Vload array. Use to calculate R0
+
 
 
 flute_id = PD.read_csv(
@@ -39,24 +46,23 @@ flute_id = PD.read_csv(
 
 # convert dictionaries from FluTe to NOMIS
 RGN17CD_df = PD.read_csv(
-    home_dir + "input_ew/RGN17CD_dict.csv",
+    nomis_dir + "RGN17CD_dict.csv",
     delimiter=',',
     delim_whitespace=False,
 )
 RGN17CD_df = RGN17CD_df.rename(columns={"Unnamed: 0": "nomis id"})
 LAD11CD_df = PD.read_csv(
-    home_dir + "input_ew/LAD11CD_dict.csv",
+    nomis_dir + "LAD11CD_dict.csv",
     delimiter=',',
     delim_whitespace=False,
 )
 LAD11CD_df = LAD11CD_df.rename(columns={"Unnamed: 0": "nomis id"})
 MSOA11CD_df = PD.read_csv(
-    home_dir + "input_ew/MSOA11CD_dict.csv",
+    nomis_dir + "MSOA11CD_dict.csv",
     delimiter=',',
     delim_whitespace=False,
 )
 MSOA11CD_df = MSOA11CD_df.rename(columns={"Unnamed: 0": "nomis id"})
-
 
 print( "-------------------------" )
 
@@ -64,15 +70,16 @@ print( "-------------------------" )
 def read_flute_data(flute_dir):
     """read in the flute log file and return it"""
     log = PD.read_csv(
-        log_file,
+        flute_dir + log_file,
         delimiter=',',
         delim_whitespace=False,
         #dtype={'':int,'':str,'':int,'':int,'':int,'':int,'':int,'':int,'':int,'':int,'':int,'':int},
     )
     log["sym0-Inf"] = log[["sym0-4","sym5-18","sym19-29","sym30-64","sym65+"]].sum(axis=1)
     log["cumsym0-Inf"] = log[["cumsym0-4","cumsym5-18","cumsym19-29","cumsym30-64","cumsym65+"]].sum(axis=1)
-    log["ninf0-Inf"] = log[["ninf0-4","ninf5-18","ninf19-29","ninf30-64","ninf65+"]].sum(axis=1)
-    log["nsus0-Inf"] = log[["nsus0-4","nsus5-18","nsus19-29","nsus30-64","nsus65+"]].sum(axis=1)
+    if not old_file_type :
+        log["ninf0-Inf"] = log[["ninf0-4","ninf5-18","ninf19-29","ninf30-64","ninf65+"]].sum(axis=1)
+        log["nsus0-Inf"] = log[["nsus0-4","nsus5-18","nsus19-29","nsus30-64","nsus65+"]].sum(axis=1)
 
     for flute_key in flute_id["TractID"].values:
         trans_key = flute_id[flute_id["TractID"] == flute_key]["FIPStract"].values[0]
@@ -237,6 +244,51 @@ def plot_beta_by_age(log):
     P.title(r"North East")
     P.legend(loc='best')
 
+def plot_Rt_by_age(log):
+    """plot beta by tract and age. NB although number is given as beta, it really should be multiplied by total population."""
+
+    time_list= N.unique(log["time"].values)
+    totals = log.groupby(["time"]).sum()
+
+    ax = P.axes()
+    ax.ticklabel_format(style='sci', scilimits=(0,0))
+    ax.plot(
+        time_list,
+        n_infected_days * totals["betasum0-4"].values/(totals["ninf0-Inf"].values),
+        label="0-4"
+        )    
+    ax.plot(
+        time_list,
+        n_infected_days * totals["betasum5-18"].values/(totals["ninf0-Inf"].values),
+        label="5-18"
+        )    
+    ax.plot(
+        time_list,
+        n_infected_days * totals["betasum19-29"].values/(totals["ninf0-Inf"].values),
+        label="19-29"
+        )    
+    ax.plot(
+        time_list,
+        n_infected_days * totals["betasum30-64"].values/(totals["ninf0-Inf"].values),
+        label="30-64"
+        )    
+    ax.plot(
+        time_list,
+        n_infected_days * totals["betasum65+"].values/(totals["ninf0-Inf"].values),
+        label="65+"
+        )   
+    ax.plot(
+        time_list,
+        n_infected_days * totals["betasum0-Inf"].values/(totals["ninf0-Inf"].values),
+        label="all"
+        )   
+    
+    P.xlim([0, 180])
+    P.xlabel(r'time  [days]', fontsize=16)
+    P.ylabel(r'Rt', fontsize=16)
+    P.ylim(bottom=0.)
+    P.title(r"North East")
+    P.legend(loc='best')
 
 def plot_withdrawn_by_age(log):
     """plot number of withdrawn individuals by tract and age"""
@@ -411,9 +463,14 @@ def make_plots(fdir):
     plot_withdrawn_by_age(log)
     P.savefig( flute_dir+"plot_withdrawn_by_age.png" )
 
-    P.figure()
-    plot_beta_by_age(log)
-    P.savefig( flute_dir+"plot_beta_by_age.png" )
+    if not old_file_type :
+        P.figure()
+        plot_beta_by_age(log)
+        P.savefig( flute_dir+"plot_beta_by_age.png" )
+
+        P.figure()
+        plot_Rt_by_age(log)
+        P.savefig( flute_dir+"plot_Rt_by_age.png" )
 
     P.figure()
     plot_susceptible_by_age(log)
